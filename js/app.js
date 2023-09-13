e8db1380d1485fd5f6fe7df004342a8e5c3ada36
Nnachijoy <nnachijoychima2000@gmail.com>
2023-09-13 14:17:16 -0700

    First Push

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".login-container");
+
+sign_up_btn.addEventListener('click', () => {
+    login-container.classList.add("sign-up-mode")
+});
+
+sign_in_btn.addEventListener('click', () => {
+    login-container.classList.remove("sign-up-mode")
+});
+
+
+function password_show_hide() {
+    var x = document.getElementById("password");
+    var show_eye = document.getElementById("show_eye");
+    var hide_eye = document.getElementById("hide_eye");
+    hide_eye.classList.remove("d-none");
+    if (x.type === "password") {
+      x.type = "text";
+      show_eye.style.display = "none";
+      hide_eye.style.display = "block";
+    } else {
+      x.type = "password";
+      show_eye.style.display = "block";
+      hide_eye.style.display = "none";
+    }
+  }
+
+
+
+
+
+
+
+
